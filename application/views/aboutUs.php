88992df011fa0da93532122439dd1df739f26514
hamshad <hiromega18@gmail.com>
2025-01-24 18:59:46 +0530

    style: about us design

--- application/views/aboutUs.php
+++ application/views/aboutUs.php
@@ -1,33 +1,30 @@
-<div class="about-us" id="contact">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="post_outer ">
-        <span class="post_loader show"></span>
+<div class="container my-3 about-us">
+  <form class="search_row py-2 px-3" method="get">
+    <h2 class="mb-3 box_heading m-0">About Us</h2>
+    <div class="row align-items-center">
+      <div class="col-12 col-sm-12 col-md-12 col-lg-12">
+        <div class="about-us-content mt-4">
+          <p>
+            Business news & in-depth insight for contractors and small and disadvantaged entrepreneurs in public works. In today's fast-changing world, we report on topics relevant to the construction, small business, and public works community. We aim to inform our readers about pressing issues facing American small businesses, contractors, subcontractors, emergent entrepreneurs, and their employees. We also report on the role of government in public works and in promoting public-private partnerships and outreach efforts. Subscribe for FREE to our weekly newsletter and contact us with any ideas or suggestions.
+          </p>
+        </div>
       </div>
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">About Us</h5>
-      </div>
-      <div class="modal-body">
-        <p>
-          Business news & in-depth insight for contractors and small and disadvantaged entrepreneurs in public works. In today's fast changing world we report on topics relevant to the construction, small business, and public works community. We aim to inform our readers about pressing issues facing American small businesses, contractors, subcontractors, emergent entrepreneurs, and their employees. We also report on the role of government in public works and in promoting public-private partnerships and outreach efforts. Subscribe for FREE to our weekly newsletter and contact us with any ideas or suggestions.
-        </p>
-      </div>
-      <script type="application/ld+json">
-        {
-          "@context": "http://schema.org",
-          "@type": "WebPage",
-          "name": "About Us",
-          "description": "Stay updated with The Tribune - your trusted source for breaking news, top headlines, in-depth analysis, and exclusive stories from India and around the world. Covering politics, sports, entertainment, business, and more.",
-          "url": "",
-          "speakable": {
-            "@type": "SpeakableSpecification",
-            "xpath": [
-              "/html/head/title",
-              "/html/head/meta[@name='description']/@content"
-            ]
-          }
-        }
-      </script>
     </div>
-  </div>
+  </form>
+  <script type="application/ld+json">
+    {
+      "@context": "http://schema.org",
+      "@type": "WebPage",
+      "name": "About Us",
+      "description": "Business news & in-depth insight for contractors and small and disadvantaged entrepreneurs in public works. In today 's fast-changing world, we report on topics relevant to the construction, small business, and public works community. We aim to inform our readers about pressing issues facing American small businesses, contractors, subcontractors, emergent entrepreneurs, and their employees. We also report on the role of government in public works and in promoting public-private partnerships and outreach efforts. Subscribe for FREE to our weekly newsletter and contact us with any ideas or suggestions.",
+      "url": "",
+      "speakable": {
+        "@type": "SpeakableSpecification",
+        "xpath": [
+          "/html/head/title",
+          "/html/head/meta[@name='description']/@content"
+        ]
+      }
+    }
+  </script>
 </div>

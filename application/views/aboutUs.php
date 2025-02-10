a207747e78c040d18bbaced5187bfba745545468
hamshad <hiromega18@gmail.com>
2025-02-10 18:34:56 +0530

    fix: aboutUs common variable

--- application/views/aboutUs.php
+++ application/views/aboutUs.php
@@ -1,3 +1,7 @@
+<?php
+$details = "Business news & in-depth insight for contractors and small and disadvantaged entrepreneurs in public works. In today's fast-changing world, we report on topics relevant to the construction, small business, and public works community. We aim to inform our readers about pressing issues facing American small businesses, contractors, subcontractors, emergent entrepreneurs, and their employees. We also report on the role of government in public works and in promoting public-private partnerships and outreach efforts. Subscribe for FREE to our weekly newsletter and contact us with any ideas or suggestions."
+?>
+
 <div class="container my-3 about-us">
   <form class="search_row py-2 px-3" method="get">
     <h2 class="mb-3 box_heading m-0">About Us</h2>
@@ -5,7 +9,7 @@
       <div class="col-12 col-sm-12 col-md-12 col-lg-12">
         <div class="about-us-content mt-4">
           <p>
-            Business news & in-depth insight for contractors and small and disadvantaged entrepreneurs in public works. In today's fast-changing world, we report on topics relevant to the construction, small business, and public works community. We aim to inform our readers about pressing issues facing American small businesses, contractors, subcontractors, emergent entrepreneurs, and their employees. We also report on the role of government in public works and in promoting public-private partnerships and outreach efforts. Subscribe for FREE to our weekly newsletter and contact us with any ideas or suggestions.
+            <?php echo $details; ?>
           </p>
         </div>
       </div>
@@ -16,7 +20,7 @@
       "@context": "http://schema.org",
       "@type": "WebPage",
       "name": "About Us",
-      "description": "Business news & in-depth insight for contractors and small and disadvantaged entrepreneurs in public works. In today 's fast-changing world, we report on topics relevant to the construction, small business, and public works community. We aim to inform our readers about pressing issues facing American small businesses, contractors, subcontractors, emergent entrepreneurs, and their employees. We also report on the role of government in public works and in promoting public-private partnerships and outreach efforts. Subscribe for FREE to our weekly newsletter and contact us with any ideas or suggestions.",
+      "description": "<?php echo $details; ?>",
       "url": "",
       "speakable": {
         "@type": "SpeakableSpecification",
